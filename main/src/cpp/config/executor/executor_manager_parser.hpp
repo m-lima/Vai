8738821540faa9ee4a226ca057b31a0b710ed8a6
Marcelo Lima <marcelo@telenordigital.com>
2018-02-25 17:32:13 +0100

    New config model decided

---
+++ main/src/cpp/config/executor/executor_manager_parser.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "executor_manager.hpp"
+#include "executor.hpp"
+
+class ExecutionManagerParser {
+private:
+  ExecutorManager executorManager;
+
+  bool parse(const std::string & line) {
+
+  }
+};
+
